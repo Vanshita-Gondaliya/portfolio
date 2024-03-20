bc56463cef34d36a9a6de5ac3d4fc939e622005c
Vanshita-Gondaliya <163412546+Vanshita-Gondaliya@users.noreply.github.com>
2024-03-20 08:29:05 +0000

    add heading

---
+++ src/app/home/profile/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './profile.scss'
+export default function Profile() {
+    return (
+        <div className="profile">
+            <div className="container">
+                <section>
+                    <div className="heading">
+                        <span>Hello I'm</span>
+                        <h1>Vanshita Gondaliya</h1>
+                        <p>Full Stack devloper</p>
+
+                    </div>
+                </section>
+            </div>
+        </div>
+    )
+}
